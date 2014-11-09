eb7cf7af0f914a6d867ec0343d02f2a277f0dce7
sha9 <b2b@live.dk>
2014-11-10 00:31:45 +0100

    First Commit/Construction

---
+++ MartialArtsAcademyV2/Controllers/AcademyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MartialArtsAcademyV2.Controllers
+{
+    public class AcademyController : Controller
+    {
+        //
+        // GET: /Academy/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Timetable()
+        {
+            return View();
+        }
+
+        public ActionResult Prices()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MartialArtsAcademyV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MartialArtsAcademyV2.MvcApplication" Language="C#" %>
